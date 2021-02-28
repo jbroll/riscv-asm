6e1e0065502d25e1327d65505fda89ecd1161b85
John Roll <john@rkroll.com>
2021-02-27 21:26:22 -0500

    unsource and test

--- elf/elf-dump.tcl
+++ elf/elf-dump.tcl
@@ -4,8 +4,15 @@ source elf.tcl
 
 set e [elf::elf create e [lindex $argv 0]]
 
-print $argv
-print [join [$e header] \n]
+set header [e header]
+dict with header {
+    print [subst {
+    $argv : $ei_class $ei_data v$ei_version type $e_type
+        Entry: [format %8X $e_entry]
+        phoff [format %8X $e_phoff] phentsize [format %4d $e_phentsize] phnum [format %4d $e_phnum] ehsize  $e_ehsize flags $e_flags
+        shoff [format %8X $e_shoff] shentsize [format %4d $e_shentsize] shnum [format %4d $e_shnum] strindx $e_shstrndx
+    }]
+}
 print
 print [table justify [$e sections]]
 print
