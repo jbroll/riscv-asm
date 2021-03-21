3ea625dd4ac9de56a409ee65f9762c6a0b76503a
John Roll <john@rkroll.com>
2021-03-21 08:11:24 -0400

    load elf to memory + fix load/store tests

--- execution.tcl
+++ execution.tcl
@@ -19,8 +19,12 @@ namespace eval ::tcl::mathfunc {
     proc ld_sbyte { addr } { expr { signed(ldb($addr), 8) } }
     proc ld_shalf { addr } { expr { signed(ldb($addr) + (ldb($addr+1) << 8), 16) } }
     proc ld_sword { addr } { expr { signed(ldb($addr) + (ldb($addr+1) << 8)  + (ldb($addr+2) << 16) + (ldb($addr+3) << 24), 32) } }
+    proc ld_sdble { addr } { expr { signed(
+        (ldb($addr  )      ) + (ldb($addr+1) <<  8)  + (ldb($addr+2) << 16) + (ldb($addr+3) << 24) +
+        (ldb($addr+4) << 32) + (ldb($addr+5) << 40)  + (ldb($addr+6) << 48) + (ldb($addr+7) << 56), 64) } }
     proc ld_ubyte { addr } { expr { ldb($addr) & 0xFF } }
     proc ld_uhalf { addr } { expr { (ldb($addr) + (ldb($addr+1) << 8)) & 0xFFFF } }
+    proc ld_uword { addr } { expr { (ldb($addr) + (ldb($addr+1) << 8)  + (ldb($addr+2) << 16) + (ldb($addr+3) << 24)) & 0xFFFFFFFF } }
 
     proc stb { value addr } {
         if { $addr < 0 || $addr >= [llength $::mem] } {
@@ -37,6 +41,15 @@ namespace eval ::tcl::mathfunc {
                                   expr { stb($value >>  8, $addr+1) }
                                   expr { stb($value >> 16, $addr+2) }
                                   expr { stb($value >> 24, $addr+3) }
+                                } 
+    proc st_dble { value addr } { expr { stb($value,       $addr)   }
+                                  expr { stb($value >>  8, $addr+1) }
+                                  expr { stb($value >> 16, $addr+2) }
+                                  expr { stb($value >> 24, $addr+3) }
+                                  expr { stb($value >> 32, $addr+4) }
+                                  expr { stb($value >> 40, $addr+5) }
+                                  expr { stb($value >> 48, $addr+6) }
+                                  expr { stb($value >> 56, $addr+7) }
                                 } 
 
     proc unsigned { value { from 31 } } {
@@ -110,6 +123,13 @@ proc elf_load_file { file } {
         }
     }
     set syms [load_syms $e]
+
+    binary scan $text cu* bytes 
+    set i 0
+    foreach byte $bytes {
+        lset ::mem $i $byte
+        incr i
+    }
 
     return [list $text $syms]
 }
@@ -220,10 +240,11 @@ proc execute { verbose args } {
                 r { read_state  .state R               ; continue }
                 d { set format $dec_format             ; continue}
                 x { set format $hex_format             ; continue}
-                q { exit }
+                q { write_state .state [array get R *] ; exit }
                 n { }
                 m { set expr $in
                     set eval [lrange $::mem {*}[lrange $in 1 end]] 
+                    continue
                 }
                 p { array set R [lpop rstack]          ; continue }
                 default {
