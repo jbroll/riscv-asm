dacf0c33f344c0241c27867ea5a38b409e278c29
John Roll <john@rkroll.com>
2021-04-25 11:07:08 -0400

    pass script and uplevel it in assert

--- test.tcl
+++ test.tcl
@@ -9,26 +9,26 @@ test::suite riscv-asm {
             source labels.tcl
         }
         case-dot {
-            eq [dot] 0                      "dot not initialized to 0"
-            eq [getlabel .] 0               "getlabel . --> 0"
+            eq {dot} 0                      "dot not initialized to 0"
+            eq {getlabel .} 0               "getlabel . --> 0"
             incrdot 4
-            eq [dot] 4                      "dot not increemnted to 4"
-            eq [getlabel .] 4               "getlabel . --> 4"
+            eq {dot} 4                      "dot not increemnted to 4"
+            eq {getlabel .} 4               "getlabel . --> 4"
 
             setlabel label wink
-            eq [getlabel label] wink        "set label to string"
+            eq {getlabel label} wink        "set label to string"
             setlabel label 3422
-            eq [getlabel label] 3422        "set label to value"
+            eq {getlabel label} 3422        "set label to value"
         }
         case-named {
             dot 14
             : here
-            eq [getlabel here] 14           "check that the label 'here' is defined"
+            eq {getlabel here} 14           "check that the label 'here' is defined"
         }
         case-unknown {
             dot 14
             here:
-            eq [getlabel here] 14           "check that the label 'here' is defined"
+            eq {getlabel here} 14           "check that the label 'here' is defined"
         }
     }
 
@@ -38,15 +38,15 @@ test::suite riscv-asm {
         }
         case-word {
             st_word 0 0x12345678
-            eq [ld_uword 0] 0x12345678     "read word back from memory"
+            eq {ld_uword 0} 0x12345678     "read word back from memory"
             binary scan [memory 0 3] i word
-            eq $word 0x12345678            "format and scan memory"
+            eq {I $word} 0x12345678            "format and scan memory"
         }
         case-half {
             st_half 6 0x5678
-            eq [ld_uhalf 6] 0x5678          "read half back from memory"
-            eq [ld_uhalf 4] 0x0             "read half back from memory 0 before"
-            eq [ld_uhalf 8] 0x0             "read half back from memory 0 after"
+            eq {ld_uhalf 6} 0x5678          "read half back from memory"
+            eq {ld_uhalf 4} 0x0             "read half back from memory 0 before"
+            eq {ld_uhalf 8} 0x0             "read half back from memory 0 after"
 
         }
     }
@@ -59,9 +59,9 @@ test::suite riscv-asm {
         }
         case {
             assemble 0x12345673 "some instrucitons"
-            eq [dot] 4                      "check that dot advances on 4 byte"
+            eq {dot} 4                      "check that dot advances on 4 byte"
             assemble 0x1234     "some other instrucitons"
-            eq [dot] 6                      "check that dot advances on 2 byte"
+            eq {dot} 6                      "check that dot advances on 2 byte"
         }
     }
 }
